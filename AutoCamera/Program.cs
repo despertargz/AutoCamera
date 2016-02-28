e9746afc0940ac759ae7ff6505c9e90e0e804ec8
Christopher <mev412@gmail.com>
2016-02-28 16:25:57 -0600

    new

---
+++ AutoCamera/Program.cs
@@ -0,0 +1,44 @@
+﻿using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AutoCamera
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hope you have a great day :)");
+            Console.WriteLine("Love you!");
+
+            FirefoxProfile profile = new FirefoxProfile(cfg("profile_dir"));
+            FirefoxDriver driver = new FirefoxDriver(profile);
+            driver.Url = cfg("url");
+            driver.Navigate();
+            driver.FindElementById("loginUserName").SendKeys(cfg("username"));
+            driver.FindElementById("loginPassword").SendKeys(cfg("password"));
+            driver.FindElementById("lalogin").Click();
+
+            var frame = driver.FindElementById("ContentFrame");
+            driver.SwitchTo().Frame(frame);
+            driver.FindElementById("play").Click();
+        }
+
+        static string cfg(string key)
+        {
+            string value = ConfigurationManager.AppSettings[key];
+            if (value == null)
+            {
+                throw new Exception("Could nto find app setting key: " + key);
+            }
+
+            return value;
+        }
+    }
+}
